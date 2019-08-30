d38d0e34762d2d9d8b573a5b52f6f82705b689a5
Echo-Gambino <nkb012pomm@gmail.com>
2019-08-29 18:00:17 -0600

    Got the header of Genda to look okay

--- cli_Genda.py
+++ cli_Genda.py
@@ -8,6 +8,10 @@ def index():
     return render_template('welcomePage.html')
 
 
+@app.route("/user")
+def main():
+    return render_template('mainPage.html')
+
 if __name__ == "__main__":
     app.run()
     pass
